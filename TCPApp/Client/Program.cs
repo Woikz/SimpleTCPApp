7f7b20f5f15cf962419e555054e8caca53b0389e
Woikz <67613092+Woikz@users.noreply.github.com>
2021-03-01 21:54:35 +0300

    Add files via upload

---
+++ TCPApp/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            NetworkStream serverStream = default(NetworkStream);
+
+            client.Connect("192.168.137.1", 8888); //Bağlantı
+            Console.WriteLine("Bağlantı Kuruldu");
+            serverStream = client.GetStream(); //Yayın Kanalına Bağlan
+
+            while (true)
+            {
+                Console.WriteLine("Mesajı Gir:");
+                string data = Console.ReadLine();
+
+                byte[] outStream = System.Text.Encoding.ASCII.GetBytes(data + "$");
+                serverStream.Write(outStream, 0, outStream.Length); //Bit Formatına çevir ve kanala yaz
+                serverStream.Flush();
+
+            }
+
+        }
+    }
+}
---
+++ TCPApp/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TCP_Chat_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TcpListener serverSocket = new TcpListener(IPAddress.Any, 8888);
+            TcpClient clientSocket = default(TcpClient);
+            serverSocket.Start();
+
+             while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+                clientSocket = serverSocket.AcceptTcpClient(); //Bir bağlantı isteği olursa kabul et
+                Console.WriteLine("Connected!");
+            }
+            
+
+            while(true){
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, 1024); //Mesajı oku
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom); //Bit formatından metine çevir
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+
+                Console.WriteLine(dataFromClient);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
